2eb3aa92ed5e715bd6cbac2c303b0b53ae1dc71d
team0 <team0.bctech@gmail.com>
2025-03-12 20:11:01 +0800

    init

--- malda_rs/src/elfs_ids.rs
+++ malda_rs/src/elfs_ids.rs
@@ -1,8 +1,8 @@
 
 pub const GET_PROOF_DATA_ELF: &[u8] = include_bytes!("../bin/get-proof-data");
-pub const GET_PROOF_DATA_ID: [u32; 8] = [0, 0, 0, 0, 0, 0, 0, 0];
+pub const GET_PROOF_DATA_ID: [u32; 8] = [1187683648, 2627568641, 2909933243, 3134434160, 4075090070, 2756518091, 1632527869, 3042494367];
 pub const GET_PROOF_DATA_PATH: &str = "../bin/get-proof-data";
 
 pub const GET_PROOF_DATA_ETHEREUM_LIGHT_CLIENT_ELF: &[u8] = include_bytes!("../bin/get_proof_data_ethereum_light_client");
-pub const GET_PROOF_DATA_ETHEREUM_LIGHT_CLIENT_ID: [u32; 8] = [0, 0, 0, 0, 0, 0, 0, 0];
+pub const GET_PROOF_DATA_ETHEREUM_LIGHT_CLIENT_ID: [u32; 8] = [562847429, 1573196772, 182963501, 3904408601, 834584332, 3154526906, 2954065826, 227321401];
 pub const GET_PROOF_DATA_ETHEREUM_LIGHT_CLIENT_PATH: &str = "../bin/get_proof_data_ethereum_light_client";
