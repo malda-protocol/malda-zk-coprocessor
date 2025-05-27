b0dcce0b7a52f8efdeff9a66184e0f4bd657df2d
Borna <94551425+Barnadrot@users.noreply.github.com>
2025-05-27 09:49:57 +0200

    Add missing markdown files

--- malda_rs/src/elfs_ids.rs
+++ malda_rs/src/elfs_ids.rs
@@ -1,3 +1,18 @@
+// Copyright (c) 2025 Merge Layers Inc.
+//
+// This source code is licensed under the Business Source License 1.1
+// (the "License"); you may not use this file except in compliance with the
+// License. You may obtain a copy of the License at
+//
+//     https://github.com/malda-protocol/malda-zk-coprocessor/blob/main/LICENSE-BSL
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// This file contains code derived from or inspired by Risc0,
+// originally licensed under the Apache License 2.0. See LICENSE-RISC0
+// and the NOTICE file for original license terms and attributions.
+
 pub const GET_PROOF_DATA_ELF: &[u8] = include_bytes!("../bin/get-proof-data.bin");
 pub const GET_PROOF_DATA_PATH: &str = "../bin/get-proof-data.bin";
 pub const GET_PROOF_DATA_ID: [u32; 8] = [1076528517, 3687338149, 4191839983, 3515197238, 4203588755, 1987859534, 436086392, 1273568593];
