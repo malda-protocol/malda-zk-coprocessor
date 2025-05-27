b0c26a19ad7239f290fd91dfff7ddadbf2e38b89
Borna <94551425+Barnadrot@users.noreply.github.com>
2025-05-27 11:41:20 +0200

    Add license headers to .rs files

--- patch/ethereum_hashing/src/sha2_impl.rs
+++ patch/ethereum_hashing/src/sha2_impl.rs
@@ -1,3 +1,18 @@
+// Copyright (c) 2025 Merge Layers Inc.
+//
+// This source code is licensed under the Business Source License 1.1
+// (the "License"); you may not use this file except in compliance with the
+// License. You may obtain a copy of the License at
+//
+//     https://github.com/malda-protocol/malda-zk-coprocessor/blob/main/LICENSE-BSL
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// This file contains code derived from or inspired by Risc0,
+// originally licensed under the Apache License 2.0. See LICENSE-RISC0
+// and the NOTICE file for original license terms and attributions.
+
 use crate::{Sha256, Sha256Context, HASH_LEN};
 use sha2::Digest;
 
