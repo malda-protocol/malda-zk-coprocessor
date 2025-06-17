ea417476afb5d6284b7584a47c13f8728562bac4
team0 <team0.bctech@gmail.com>
2025-06-17 09:55:31 +0300

    update zkvm

--- malda_rs/src/elfs_ids.rs
+++ malda_rs/src/elfs_ids.rs
@@ -1,19 +1,6 @@
-// Copyright (c) 2025 Merge Layers Inc.
-//
-// This source code is licensed under the Business Source License 1.1
-// (the "License"); you may not use this file except in compliance with the
-// License. You may obtain a copy of the License at
-//
-//     https://github.com/malda-protocol/malda-zk-coprocessor/blob/main/LICENSE-BSL
-//
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-//
-
 pub const GET_PROOF_DATA_ELF: &[u8] = include_bytes!("../bin/get-proof-data.bin");
 pub const GET_PROOF_DATA_PATH: &str = "../bin/get-proof-data.bin";
-pub const GET_PROOF_DATA_ID: [u32; 8] = [1076528517, 3687338149, 4191839983, 3515197238, 4203588755, 1987859534, 436086392, 1273568593];
+pub const GET_PROOF_DATA_ID: [u32; 8] = [3789439100, 2194748007, 2821065651, 2223447387, 2386557686, 2544425010, 1947704562, 4067243773];
 pub const GET_PROOF_DATA_ETHEREUM_LIGHT_CLIENT_ELF: &[u8] = include_bytes!("../bin/get_proof_data_ethereum_light_client.bin");
 pub const GET_PROOF_DATA_ETHEREUM_LIGHT_CLIENT_PATH: &str = "../bin/get_proof_data_ethereum_light_client.bin";
-pub const GET_PROOF_DATA_ETHEREUM_LIGHT_CLIENT_ID: [u32; 8] = [4132944194, 3658945254, 2459060084, 1048260215, 3272341521, 4046998054, 3851597343, 4248394182];
+pub const GET_PROOF_DATA_ETHEREUM_LIGHT_CLIENT_ID: [u32; 8] = [665074516, 4151713324, 2337823910, 4098924883, 2749032413, 1495710693, 1570817937, 3826476548];
