73964a49cf89390060670b4416a3dc6d9e459f1d
Andrzej Broński <andrzej1_1@o2.pl>
2026-01-20 09:13:30 +0100

    Allow building guest with mainnet/testnet feature.
    We can now build guests in a fully automated way, without
    commenting/uncommenting the relevant checks manually.

--- methods/guest/src/bin/get_proof_data.rs
+++ methods/guest/src/bin/get_proof_data.rs
@@ -18,7 +18,13 @@ use risc0_op_steel::optimism::OpEvmInput;
 use risc0_zkvm::guest::env;
 use alloy_consensus::Header;
 use alloy_sol_types::SolValue;
-use malda_utils::constants::{LINEA_CHAIN_ID, BASE_CHAIN_ID, ETHEREUM_CHAIN_ID, LINEA_SEPOLIA_CHAIN_ID, BASE_SEPOLIA_CHAIN_ID, ETHEREUM_SEPOLIA_CHAIN_ID, OPTIMISM_CHAIN_ID, OPTIMISM_SEPOLIA_CHAIN_ID};
+#[cfg(not(any(feature = "mainnet", feature = "testnet")))]
+compile_error!("Either 'mainnet' or 'testnet' feature must be enabled. Build with --features mainnet or --features testnet");
+
+#[cfg(feature = "mainnet")]
+use malda_utils::constants::{LINEA_CHAIN_ID, BASE_CHAIN_ID, ETHEREUM_CHAIN_ID, OPTIMISM_CHAIN_ID};
+#[cfg(feature = "testnet")]
+use malda_utils::constants::{LINEA_SEPOLIA_CHAIN_ID, BASE_SEPOLIA_CHAIN_ID, ETHEREUM_SEPOLIA_CHAIN_ID, OPTIMISM_SEPOLIA_CHAIN_ID};
 
 fn main() {
     let mut output: Vec<Bytes> = Vec::new();
@@ -39,12 +45,12 @@ fn main() {
         let env_op_input_2: Option<EthEvmInput> = env::read();
         let linea_beacon_data: Option<linea_block_verifier::core::types::BeaconData> = env::read();
 
-        // This makes the guest program only compatible with mainnet chains, remove for testnet and enable the below testnet code
-        // if chain_id != LINEA_CHAIN_ID && chain_id != BASE_CHAIN_ID && chain_id != ETHEREUM_CHAIN_ID && chain_id != OPTIMISM_CHAIN_ID {
-        //     panic!("Chain ID is not Linea, Base, Ethereum or Optimism");
-        // }
+        #[cfg(feature = "mainnet")]
+        if chain_id != LINEA_CHAIN_ID && chain_id != BASE_CHAIN_ID && chain_id != ETHEREUM_CHAIN_ID && chain_id != OPTIMISM_CHAIN_ID {
+            panic!("Chain ID is not Linea, Base, Ethereum or Optimism");
+        }
 
-        // This makes the guest program only compatible with testnet chains, remove for mainnet and enable the above mainnet code
+        #[cfg(feature = "testnet")]
         if chain_id != LINEA_SEPOLIA_CHAIN_ID && chain_id != BASE_SEPOLIA_CHAIN_ID && chain_id != ETHEREUM_SEPOLIA_CHAIN_ID && chain_id != OPTIMISM_SEPOLIA_CHAIN_ID {
             panic!("Chain ID is not Linea Sepolia, Base Sepolia, Ethereum Sepolia or Optimism Sepolia");
         }
