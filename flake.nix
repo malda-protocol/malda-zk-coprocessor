e36bc63ffd12132aa936666e8f50c6f5a8e8cc53
Andrzej Broński <koxu1996@gmail.com>
2026-02-13 16:30:20 +0100

    Separate vulnerabilities ignored for guest.

--- flake.nix
+++ flake.nix
@@ -56,8 +56,9 @@
               #   RUSTSEC-2023-0071 (rsa) - see https://github.com/malda-protocol/malda-zk-coprocessor/issues/39
               #   RUSTSEC-2025-0055 (tracing-subscriber) - see https://github.com/malda-protocol/malda-zk-coprocessor/issues/40
               IGNORE="--ignore RUSTSEC-2023-0071 --ignore RUSTSEC-2025-0055"
+              GUEST_IGNORE="--ignore RUSTSEC-2025-0055"
               cargo-audit audit --no-fetch $IGNORE --db ${advisory-db} --file ${./Cargo.lock}
-              cargo-audit audit --no-fetch $IGNORE --db ${advisory-db} --file ${./methods/guest/Cargo.lock}
+              cargo-audit audit --no-fetch $GUEST_IGNORE --db ${advisory-db} --file ${./methods/guest/Cargo.lock}
               touch $out
             '';
       });
